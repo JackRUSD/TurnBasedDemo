064b5bd74bc067b44fa2f5f553fcb494f41abc3a
JackRettRUSD <jerettinhouse@rocklinusd.org>
2020-01-08 15:49:13 -0800

    Some code

---
+++ Assets/Scenes/Tiles/Material/Resources/AssignTileMaterial.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+   
+
+public class MenuScript
+{
+    [MenuItem("Tools/Assign Material")]
+    public static void AssignMaterial()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        Material material = Resources.Load<Material>("Tile");
+
+        foreach (GameObject t in tiles)
+        {
+            t.GetComponent<Renderer>().material = material;
+        }
+    }
+
+    [MenuItem("Tools/Assign Tile Script")]
+   
+    public static void AssignScript()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+
+        foreach (GameObject t in tiles)
+        {
+            t.AddComponent<AssignTileMaterial>();
+        }
+    }
+}
